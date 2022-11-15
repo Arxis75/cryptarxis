bd2967fda0fbc3d9c405687f495de2c61bb62f61
Arxis75 <a.b@c.com>
2022-11-15 10:59:48 +0100

    first commit

---
+++ crypto/secp256k1/EllipticCurve.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class EllipticCurve
+{
+private:
+    typedef ZP::Element Element;
+    Curve *curve;
+    ZP *FField;
+    Point identity;
+public:
+    EllipticCurve(Curve *c);
+    EllipticCurve(Integer primeField, Integer A, Integer B);
+    ~EllipticCurve();
+
+    const Point& _inv(Point& Q, const Point& P);
+    bool _isInv(const Point& Q, const Point& P);
+    Point& _double(Point& R, const Point& P);
+    Point& _add(Point&R, const Point &P, const Point& Q);
+    Point& _scalar(Point& R, const Point& P,Integer k);
+
+    bool verifyPoint(const Point& P) const;
+    void print();
+
+};
